72c3a2b3c373fd9c3a29aa1bd48931025a6e4e3c
Quy Nguyen <mvpvn.official@gmail.com>
2022-06-02 10:34:21 +0000

    Task 4: Upload the Docker Image

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=tanquyvt/udacity-api
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag api $dockerpath:api-v1
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:api-v1
